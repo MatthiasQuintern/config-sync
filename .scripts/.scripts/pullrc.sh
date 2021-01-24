4d66709c4a8a5781b2fd0125e4dc698fbc7d47e6
matthias@arch <matthiasqui@protonmail.com>
2021-01-24 03:57:49 +0100

    Update über importrc.sh

---
+++ .scripts/.scripts/pullrc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# exit wenn ein programm nicht mit 0 exited
+set -e
+
+cd ~/Sync
+echo "Pull von Github"
+    git pull --no-rebase origin master
+
+echo "Fertig!"
+
+echo "Kopiere Dateien ins Home Verzeichnis"
+    # damit * auch versteckte Dateien findet
+    shopt -s dotglob
+    cp -R * ~/
+
+echo "Fertig!"
